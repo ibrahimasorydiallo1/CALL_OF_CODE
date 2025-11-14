27e6d2742fdeaa6b002144809902c28d2414ffdf
ibrahimasorydiallo1 <isddiallo2003@gmail.com>
2025-11-14 11:14:06 +0100

    affichage du csv

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,3 +4,7 @@ import pandas as pd
 st.set_page_config(page_title="Data analyse", page_icon="🌕", layout="wide")
 
 st.title("Call Of Code ⚔️")
+
+df_csv = pd.read_csv("app\\assets\production_2025_10.csv", sep=";")
+st.write("Ceci est le csv affiché")
+st.write(df_csv)
