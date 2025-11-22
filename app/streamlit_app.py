f3e27a94f0ca98ab60f799f0f8671e6f83f4daec
ibrahimasorydiallo1 <isddiallo2003@gmail.com>
2025-11-22 11:15:45 +0100

    connection ok avec Postgres

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -5,6 +5,15 @@ st.set_page_config(page_title="Data analyse", page_icon="🌕", layout="wide")
 
 st.title("Call Of Code ⚔️")
 
-df_csv = pd.read_csv("app\\assets\production_2025_10.csv", sep=";")
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+st.subheader("Table turbine")
+df = conn.query('SELECT * FROM turbine;', ttl="10m")
+
+st.write(df)
+
+df_csv = pd.read_csv("app\production_2025_10.csv", sep=";")
 st.write("Ceci est le csv affiché")
 st.write(df_csv)
